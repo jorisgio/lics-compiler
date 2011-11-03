fdef9bf809f57c5251cd7d084a14b2548e0b3b27
Joris GIOVANNANGELI <joris.giovannangeli@>
2011-11-03 14:25:12 +0100

    Ajout des types des représentations intermédiaires

--- src/graphe.mli
+++ src/graphe.mli
@@ -9,3 +9,10 @@ type label = Noeud.label
 module Graphe = Builder.MakeLabeledGraph(Noued)
 module Vertex = Graphe.Vertex
 
+(*type renvoyé après la pemière passe (aka la construction du graphe) *)
+type interm_graph = { graph : Graphe.t; inputs : Noeud.t list; outputs : Noeud.t list}
+
+(* type renvoyé après la suppression des registres *)
+type combin_graph = { graph : Graphe.t; inputs : Noeud.t list; outputs : Noeud.t list; inregs : Noeud.t list; outregs : Noeut.t list}
+
+(* Type renvoyé par le tri topo *)
