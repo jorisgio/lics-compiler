06a6e7f9654f806a453751da0a4f7373e54f550e
Joris GIOVANNANGELI <joris.giovannangeli@>
2011-11-10 18:26:36 +0100

    Changement d'interface dans Ast.mli et Graphe.ml
    Attention, BREAKING API ! Caml ne supporte pas des enregistrements ayant des identifiers de même nom dans le même namespace. Il est en effet impossible d'inéferer le type de certaines fonction.
    ex :
    type A = { a : int}
    type B = { a :int; b : bool }
    let foo x = x.a
    Impossible d'inférer !
    Changement de tout les noms des attributs pour respecter cette règle.

--- src/graphe.ml
+++ src/graphe.ml
@@ -1,18 +1,18 @@
 exception Not_implemented
+
 module Noeud = struct
 	type t = int
 	type label = Empty | And | Or | Not | Xor | Reg | Input | Output | Inreg | Outreg |  True | False
 	let compare = Pervasives.compare
 	let empty = Empty
 end
-type label = Noeud.label
 module Graphe = Builder.MakeLabeledGraph(Noeud)
 module Vertex = Graphe.Vertex
 
 (*type renvoyé après la pemière passe (aka la construction du graphe) *)
-type interm_graph = { graph : Graphe.t; inputs : Noeud.t list; outputs : Noeud.t list}
+type interm_graph = { igraph : Graphe.t; iinputs : Noeud.t list; ioutputs : Noeud.t list}
 
 (* type renvoyé après la suppression des registres *)
-type combin_graph = { graph : Graphe.t; inputs : Noeud.t list; outputs : Noeud.t list; inregs : Noeud.t list; outregs : Noeud.t list}
+type combin_graph = { cgraph : Graphe.t; cinputs : Noeud.t list; coutputs : Noeud.t list; cinregs : Noeud.t list; coutregs : Noeud.t list}
 
 (* Type renvoyé par le tri topo *)
