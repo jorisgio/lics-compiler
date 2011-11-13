6070a840069ec9840f3962c3b03d2f4c27b97a7d
Théo Zimmermann <theo.zimmermann@ens.fr>
2011-11-13 18:45:39 +0100

    Utilisation partielle de la lib

--- src/translator.ml
+++ src/translator.ml
@@ -1,25 +1,7 @@
 
 open Graphe
 open TopoSort
-
-(* types de description des actions à exécuter *)
-
-type unaire = Not
-type binaire = Or | And | Nand | Xor
-type ternaire = Mux
-
-type expression =
-    Const of bool
-  | Unaire of unaire * int
-  | Binaire of binaire * int * int
-  | Ternaire of ternaire * int * int * int
-
-type stmt =
-    Assign of int * expression
-  | Inputreg of int | Input of int
-  | Outputreg of int | Output of int
-
-type program = stmt list
+open LicsAst
 
 (* Pour produire du Lics *)
 let op_to_string = function
