d6f178aa61702a5cebb148672bf435386c301939
Joris GIOVANNANGELI <joris.giovannangeli@>
2011-12-15 15:51:32 +0100

    Changement de undefSet par une undefMap

--- src/semantic.ml
+++ src/semantic.ml
@@ -62,7 +62,8 @@ module InstrToSast = struct
       | EArray_i(name,index) -> 
 	(* on cherche le tableau dans l'env, forcément déclaré *)
 	let id = Smap.find name env in
-	if index >= size then (raise (Error(posToSast expr.p,"Bad index"))) ;
+	let Sast.Array(size) = id.Sast.typ in
+	if index >= size then (raise (Error(posToSast exp.p,"Bad index"))) ;
 	(* on vérifie que l'index est défini, sinon on le marque undef *)
 	let ar = Smap.find name undefMap in
 	let undefMap = 
@@ -76,7 +77,7 @@ module InstrToSast = struct
 	  
       | EArray_r(name,i_beg,i_end) ->
 	let id = Smap.find name env in 
-	let Array(size) = id.typ in
+	let Sast.Array(size) = id.Sast.typ in
 	(* on vérfie que l'index est correct *)
 	if i_beg < 0 or i_end < 0 or i_end < i_beg or i_end >= size then (raise (Error(posToSast exp.p,"Bad index")));
 	
@@ -84,9 +85,9 @@ module InstrToSast = struct
 	let ar = Smap.find name undefMap in
 	for i = i_beg to i_end do
 	  if not (ar.(i) = 1) then 
-	    (ar.(index) <- 2) ;
+	    (ar.(i) <- 2) ;
 	done ;
-	({ Sast.p = posToSast exp.p; Sast.e = Sast.EArray_r(idToSast id,i_beg,i_end); Sast.t = Sast.Array(i_end - i_beg)},(Smap.add ar undefMap))
+	({ Sast.p = posToSast exp.p; Sast.e = Sast.EArray_r(id,i_beg,i_end); Sast.t = Sast.Array(i_end - i_beg)},(Smap.add name ar undefMap))
 	  
       | EVar(id) ->     
 	let {Sast.typ = typ},map = 
@@ -97,7 +98,7 @@ module InstrToSast = struct
 	(({Sast.p = posToSast exp.p; Sast.e = Sast.EVar(idToSast id) ; Sast.t = typ}),map)
 	  
       | EPrefix(p,ex) -> 
-	let e,set = pExpr env undefSet ex in
+	let e,undefMap = pExpr env undefMap ex in
 	let ty,sp =
 	  match p with
 	    | Minus -> if e.Sast.t != Sast.Int then 
@@ -113,10 +114,10 @@ module InstrToSast = struct
 	      else
 		Sast.Bool,Sast.Reg
 	in
-	({ Sast.p = posToSast exp.p ; e = Sast.EPrefix(prefixToSast p,e); t = ty},set)
+	({ Sast.p = posToSast exp.p ; e = Sast.EPrefix(prefixToSast p,e); t = ty},undefMap)
       | EInfix(i,ex1,ex2) -> 
-	let e1,s1 = pExpr env undefSet ex1 in
-	let e2,s2 = pExpr env undefSet ex2 in
+	let e1,undefMap = pExpr env undefMap ex1 in
+	let e2,undefMap = pExpr env undefMap ex2 in
 	let ty,si =
 	  match i with 
 	    | Add -> Sast.Int,Sast.Add
@@ -130,7 +131,7 @@ module InstrToSast = struct
 	if e1.Sast.t != ty or e2.Sast.t != ty then
 	  (raise (WrongType(e1.Sast.p,e1.Sast.t,ty)))
 	else
-	  ({Sast.p = posToSast exp.p ; Sast.e = Sast.EInfix(si,e1,e2); Sast.t = ty},(Sset.union s1 s2))
+	  ({Sast.p = posToSast exp.p ; Sast.e = Sast.EInfix(si,e1,e2); Sast.t = ty},(undefMap))
       | EMux(_,_,_) -> failwith "Not implemented"
 
 
@@ -147,13 +148,13 @@ module InstrToSast = struct
   let rec pInstr env undefMap inst = 
     match inst.i with 
       | Assign(id,exp) -> 
-	let e,undefMap = pExpr env undefSet exp in
+	let e,undefMap = pExpr env undefMap exp in
 	let id = idToSast id in
 	if id.Sast.typ != e.Sast.t then
 	  (raise   (WrongType(e.Sast.p,e.Sast.t,id.Sast.typ)))
 	else 
 	  ({Sast.posi = posToSast inst.posi; Sast.i = Sast.Assign(id,e)},
-	   (Smap.add name [|1|] undefMap),
+	   (Smap.add id.Sast.id [|1|] undefMap),
 	   (Smap.add id.Sast.id {Sast.id = id.Sast.id; Sast.typ = id.Sast.typ} env ))
     (*  | For(i,ex1,ex2,li) ->
 	let inst2,undefSet,tmpEnv =
@@ -183,13 +184,12 @@ module InstrToSast = struct
       | Assign_i(name, index, exp) -> 
 	let e,undefMap = pExpr env undefMap exp in
 	let id = Smap.find name env in
-	let Sast.Array(size) = id.Sast.id in
 	let ar = Smap.find name undefMap in
 	ar.(index) <- 1 ;
 	if e.Sast.t != Sast.Bool then
 	  (raise   (WrongType(e.Sast.p,e.Sast.t,Sast.Bool)))
 	else 
-	  ({Sast.posi = posToSast inst.posi; Sast.i = Sast.Assign_i(id,e)},
+	  ({Sast.posi = posToSast inst.posi; Sast.i = Sast.Assign_i(id,index,e)},
 	   (Smap.add name ar undefMap),
 	   (Smap.add id.Sast.id {Sast.id = id.Sast.id; Sast.typ = id.Sast.typ} env ))
 	    
@@ -262,7 +262,7 @@ module GatesToSast = struct
        Renvoit :
        accList * accSize*)
     let checkOutputs (accList,size) (expr : Past.expr) =
-      let expr,_ = InstrToSast.pExpr Smap.empty Sset.empty expr in
+      let expr,_ = InstrToSast.pExpr Smap.empty Smap.empty expr in
       match expr.e with
 	| EVar(ident) -> 
 	  let incr = match ident.typ with
@@ -280,10 +280,25 @@ module GatesToSast = struct
     let outputs,size = List.fold_left checkOutputs ([],0) gate.Past.goutputs in
     (* création d'un env contenant toutes les entrées *)
     let env = List.fold_left (fun env id -> Smap.add id.id id env)  Smap.empty  inputs in
+    (* on ajoute  les entrées au undefMap *)
+    let undefMap = List.fold_left (fun map id -> 
+      let ar =
+	match id.typ with
+	  | Array s -> Array.make s 1 
+	  | Bool -> [|1|]
+      in
+      Smap.add id.id ar map) Smap.empty inputs in
+    
     (*Typage du corps, vérifit que toutes les variables utilisées sont définies.
       Renvoit l'env des variables locales. *)
-    let body,undefSet,env = InstrToSast.pInstrList env Sset.empty [] gate.Past.gbody in
-    if not (Sset.is_empty undefSet ) then (raise (Error({line = 0; char_b = 0; char_e = 0},"Use of unitialised value"))) ;
+    let body,undefMap,env = InstrToSast.pInstrList env undefMap [] gate.Past.gbody in
+    (* On vérifie que tout ce qui est utilisé est défini *)
+    let _ = Smap.iter (fun name ar ->
+      for i = 0 to (Array.length ar) -1 do
+	if ar.(i) > 1 then (raise (Undefined)) ;
+      done ;
+    ) undefMap 
+    in	  
     { gname = gate.Past.gname; genv = env ; ginputs = inputs ; gbody = body; goutputs = outputs ; goutputsize = size ; ginputsize = !inputsize }
 
 
@@ -304,7 +319,7 @@ module CircuitToSast = struct
   let pBlock {Past.bname = name ; bgate_type = typ ; binputs = inputs} =
     {Sast.bname = name ;
      bgate_type = typ ;
-     binputs = List.map (function expr -> fst (InstrToSast.pExpr Smap.empty Sset.empty expr)) inputs} (* Est-ce la bonne méthode ? *)
+     binputs = List.map (function expr -> fst (InstrToSast.pExpr Smap.empty Smap.empty expr)) inputs} (* Est-ce la bonne méthode ? *)
   
   let pCircuit {Past.gates = gates ; blocks = blocks} =
     {Sast.gates = GatesToSast.buildMap (List.map GatesToSast.pGate gates) ; blocks = List.map pBlock blocks}
